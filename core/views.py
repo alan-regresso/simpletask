37730ad5491a9f700271ffefdef3c2ebf3cecc91
alan freitas <alan.vitor@escolar.ifrn.edu.br>
2026-03-24 16:17:12 -0300

    Simpletask

--- core/views.py
+++ core/views.py
@@ -1,35 +1,44 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Task 
-from .forms import TaskForm
+from .models import Task, Project
+from .forms import TaskForm, ProjectForm
 
 def lista_tarefas(request):
-    tarefas = Task.objects.all()
-    return render(request, 'tasks/list.html', {'tarefas': tarefas})
+tarefas = Task.objects.all()
+return render(request, 'tasks/list.html', {'tarefas': tarefas})
 
 def criar_tarefa(request):
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()  
-            return redirect('lista_tarefas')
-    else:
-        form = TaskForm()
-    return render(request, 'tasks/form.html', {'form': form})
+if request.method == 'POST':
+form = TaskForm(request.POST)
+if form.is_valid():
+form.save()
+return redirect('lista_tarefas')
+else:
+form = TaskForm()
+
+projetos = Project.objects.all()
+return render(request, 'tasks/form.html', {'form': form, 'projetos': projetos})
 
 def editar_tarefa(request, id):
-    tarefa = get_object_or_404(Task, id=id)
-    if request.method == 'POST':
-        form = TaskForm(request.POST, instance=tarefa)
-        if form.is_valid():
-            form.save()  
-            return redirect('lista_tarefas')
-    else:
-        form = TaskForm(instance=tarefa)
-    return render(request, 'tasks/form.html', {'form': form})
+tarefa = get_object_or_404(Task, id=id)
+if request.method == 'POST':
+form = TaskForm(request.POST, instance=tarefa)
+if form.is_valid():
+form.save()
+return redirect('lista_tarefas')
+else:
+form = TaskForm(instance=tarefa)
+return render(request, 'tasks/form.html', {'form': form})
 
-def excluir_tarefa(request, id):
-    tarefa = get_object_or_404(Task, id=id)
-    if request.method == 'POST':
-        tarefa.delete()  
-        return redirect('lista_tarefas')
-    return render(request, 'tasks/list.html', {'tarefa': tarefa})
+def lista_projetos(request):
+projetos = Project.objects.all()
+return render(request, 'projects/list.html', {'projetos': projetos})
+
+def criar_projeto(request):
+if request.method == 'POST':
+form = ProjectForm(request.POST)
+if form.is_valid():
+form.save()
+return redirect('lista_projetos')
+else:
+form = ProjectForm()
+return render(request, 'projects/form.html', {'form': form})
